0e007dfc1e0ead9935d6d5274ee719854bc6e3e7
thuylawrence <thuylawrence@byui.edu>
2023-11-29 18:46:58 +0800

    update swagger.js

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
+
+// Middleware to check if user is authenticated
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+
 
 module.exports = router;
